0000e2089333aa3da1ecaed79dcb720536310c32
Cobalt <Chaosthe0rie@protonmail.com>
2020-04-08 01:13:03 +0200

    improve log in  and add support for electron-forge deb making

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from 'path';
-import { RpcClient } from 'jsonrpc-ts';
 const axios = require('axios');
+const keytar = require('keytar');
 const url = "https://www.schulerzbistum.de/jsonrpc.php";
-
+import { Config } from './store.js';
 
 declare global {
   namespace NodeJS {
@@ -12,9 +12,20 @@ declare global {
        tmp: any;
        user: any;
        member: any[];
+       config: any;
     }
   }
 }
+
+// First instantiate the class
+global.config = new Config({
+  // We'll call our data file 'user-preferences'
+  configName: 'user-preferences',
+  defaults: {
+    "email": null
+  }
+});
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -31,13 +42,13 @@ const createWindow = () => {
     }
   });
 
-  // maximize window
+  // maximize window (bootstrap does scaling if resized)
   mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
 
-  // Open the DevTools. (toggle if required)
+  // Open the DevTools. (uncomment if required)
   // mainWindow.webContents.openDevTools();
 };
 
@@ -58,36 +69,54 @@ app.on('window-all-closed', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 ipcMain.on("log-in", async function log_in(event: any, args: any) {
-let data = JSON.stringify([{
-"jsonrpc": "2.0",
-"id": 1,
-"method": "login",
-"params": {login: args["e-mail"], password: args["password"]}
-}]);
-var fallback;
+let data = JSON.stringify([
+  {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "login",
+    "params": {
+      login: args["e-mail"], password: args["password"]
+    }
+  },
+  {
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "set_focus",
+    "params": {
+      "object": "messenger",
+    }},
+  {
+    "jsonrpc": "2.0",
+    "id": 3,
+    "method": "read_quick_messages"
+  }
+]);
 axios
 .post(url, data)
 .then((result: any) => {
-try {
-  if (result.data[0].result.return != "FATAL") {
-   global.response = result.data[0].result;
-   global.member = result.data[0].result.member;
-   global.user = result.data[0].result.user;
-   BrowserWindow.getFocusedWindow().loadFile(path.join(__dirname, '../src/main.html'));
-
-  }
-// fallback
-} catch(error) {
-  console.log(error);
-  fallback = true;
-}})
+  try {
+    if (result.data[0].result.return != "FATAL") {
+     global.response = result.data[0].result;
+     global.member = result.data[0].result.member;
+     global.user = result.data[0].result.user;
+     if (args["remember-me"]) {
+       console.log("adding creds …")
+       global.config.set("e-mail", args["e-mail"])
+       keytar.addPassword('Argon Credentials', args["e-mail"], args["password"]);
+     }
+     BrowserWindow.getFocusedWindow().loadFile(path.join(__dirname, '../src/main.html'));
+   } else {
+     event.sender.send("form-recieved", {"has_errors": true});
+   }
+  // fallback = error or false creds
+  } catch(error) {
+    console.log(error);
+    event.sender.send("internal-error");
+  }})
 .catch((error: any) => {
   console.log(error);
-  fallback = true;
+  event.sender.send("internal-error");
 });
-if (fallback) {
-  event.sender.send("form-recieved", {"has_errors": true});
-}
 });
 
 
--- src/specific/index.js
+++ src/specific/index.js
@@ -1,17 +1,43 @@
 'use strict'
 
-const { remote, ipcRenderer} = require("electron");
+const { remote, ipcRenderer, shell} = require("electron");
 const $ = require('jquery');
 const btntext = $( "#submit" ).text();
+const config = remote.getGlobal("config");
+const email = config.get("e-mail");
 
 $( '#has_error' ).hide();
 
+if (email != null) {
+  $( "e-mail" ).val(email);
+}
+
+//open home page externally (Yes. Custom support for my homepage)
+$('a').click(function (event) {
+    event.preventDefault();
+    shell.openExternal(this.href);
+});
+
 $( "#log-in" ).submit(function (event) {
-  $( "#submit" ).innerHTML  = "<i class='fas fa-cog fa-spin'></i>" + btntext;
+  $( "#submit" ).innerHTML  = "<i class='fas fa-cog fa-spin' style='fill: #fff;' class='white-text'></i>" + btntext;
   event.preventDefault();
   ipcRenderer.send("log-in",
                        {"e-mail": $( '#e-mail' ).val(),
-                        "password": $( '#password' ).val()});
+                        "password": $( '#password' ).val(),
+                        "remember-me": $( '#remember-me' ).val()});
+});
+
+ipcRenderer.on('internal-error', function (event, args) {
+  $( "#submit" ).text(btntext);
+  var tmp = $("#has_error").html();
+  $("#has_error").html("Internal error - For help please <a href='https://sinclair.gq/contact' class='text-muted'>Contact me</a>");
+  $("#has_error").show();
+  setTimeout(function() {$("#has_error").addClass("fadeOut");}, 4000);
+  setTimeout(
+    function () {
+      $("#has_error").hide();
+  }, 4500);
+  $("#has_error").html();
 });
 
 ipcRenderer.on('form-recieved', function(event, args) {
